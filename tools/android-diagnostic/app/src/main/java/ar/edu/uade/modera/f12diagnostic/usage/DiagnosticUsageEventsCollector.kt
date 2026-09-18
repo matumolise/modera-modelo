8271359b9d25693ac5235983fde0aee97a9609ad
Matias Molise <matumolise@gmail.com>
2026-09-18 12:06:37 -0300

    Fix UsageEvents type labels for modern Android

--- tools/android-diagnostic/app/src/main/java/ar/edu/uade/modera/f12diagnostic/usage/DiagnosticUsageEventsCollector.kt
+++ tools/android-diagnostic/app/src/main/java/ar/edu/uade/modera/f12diagnostic/usage/DiagnosticUsageEventsCollector.kt
@@ -218,6 +218,28 @@ class DiagnosticUsageEventsCollector(
 
     @Suppress("DEPRECATION")
     private fun eventTypeName(eventType: Int): String {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            return when (eventType) {
+                UsageEvents.Event.ACTIVITY_RESUMED -> "ACTIVITY_RESUMED"
+                UsageEvents.Event.ACTIVITY_PAUSED -> "ACTIVITY_PAUSED"
+                UsageEvents.Event.ACTIVITY_STOPPED -> "ACTIVITY_STOPPED"
+                UsageEvents.Event.FOREGROUND_SERVICE_START -> "FOREGROUND_SERVICE_START"
+                UsageEvents.Event.FOREGROUND_SERVICE_STOP -> "FOREGROUND_SERVICE_STOP"
+                UsageEvents.Event.DEVICE_STARTUP -> "DEVICE_STARTUP"
+                UsageEvents.Event.DEVICE_SHUTDOWN -> "DEVICE_SHUTDOWN"
+                UsageEvents.Event.CONFIGURATION_CHANGE -> "CONFIGURATION_CHANGE"
+                UsageEvents.Event.USER_INTERACTION -> "USER_INTERACTION"
+                UsageEvents.Event.SHORTCUT_INVOCATION -> "SHORTCUT_INVOCATION"
+                UsageEvents.Event.STANDBY_BUCKET_CHANGED -> "STANDBY_BUCKET_CHANGED"
+                UsageEvents.Event.SCREEN_INTERACTIVE -> "SCREEN_INTERACTIVE"
+                UsageEvents.Event.SCREEN_NON_INTERACTIVE -> "SCREEN_NON_INTERACTIVE"
+                UsageEvents.Event.KEYGUARD_SHOWN -> "KEYGUARD_SHOWN"
+                UsageEvents.Event.KEYGUARD_HIDDEN -> "KEYGUARD_HIDDEN"
+                UsageEvents.Event.NONE -> "NONE"
+                else -> "EVENT_TYPE_$eventType"
+            }
+        }
+
         return when (eventType) {
             UsageEvents.Event.SCREEN_INTERACTIVE -> "SCREEN_INTERACTIVE"
             UsageEvents.Event.SCREEN_NON_INTERACTIVE -> "SCREEN_NON_INTERACTIVE"
@@ -225,21 +247,11 @@ class DiagnosticUsageEventsCollector(
             UsageEvents.Event.KEYGUARD_HIDDEN -> "KEYGUARD_HIDDEN"
             UsageEvents.Event.MOVE_TO_FOREGROUND -> "MOVE_TO_FOREGROUND"
             UsageEvents.Event.MOVE_TO_BACKGROUND -> "MOVE_TO_BACKGROUND"
-            else -> eventTypeNameApi29Plus(eventType)
-        }
-    }
-
-    private fun eventTypeNameApi29Plus(eventType: Int): String {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
-            return "EVENT_TYPE_$eventType"
-        }
-
-        return when (eventType) {
-            UsageEvents.Event.ACTIVITY_RESUMED -> "ACTIVITY_RESUMED"
-            UsageEvents.Event.ACTIVITY_PAUSED -> "ACTIVITY_PAUSED"
-            UsageEvents.Event.ACTIVITY_STOPPED -> "ACTIVITY_STOPPED"
-            UsageEvents.Event.DEVICE_STARTUP -> "DEVICE_STARTUP"
-            UsageEvents.Event.DEVICE_SHUTDOWN -> "DEVICE_SHUTDOWN"
+            UsageEvents.Event.CONFIGURATION_CHANGE -> "CONFIGURATION_CHANGE"
+            UsageEvents.Event.USER_INTERACTION -> "USER_INTERACTION"
+            UsageEvents.Event.SHORTCUT_INVOCATION -> "SHORTCUT_INVOCATION"
+            UsageEvents.Event.STANDBY_BUCKET_CHANGED -> "STANDBY_BUCKET_CHANGED"
+            UsageEvents.Event.NONE -> "NONE"
             else -> "EVENT_TYPE_$eventType"
         }
     }
